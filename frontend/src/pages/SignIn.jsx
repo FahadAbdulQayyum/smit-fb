fffaf3b4cfabb97bc49331da52263e279fff3eb5
Fahad <fahad.45854@iqra.edu.com>
2024-07-10 20:42:05 +0500

    Sign In text corrected

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -71,58 +71,58 @@ export default function SignIn() {
     }
   };
   return (
-    <div className="min-h-screen mt-20">
-      <div className="flex p-3 max-w-5xl mx-auto flex-col md:flex-row md:items-center gap-5">
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-5xl mx-auto flex-col md:flex-row md:items-center gap-5'>
         {/* left */}
 
-        <div className="flex-grow">
-          <img src="/smit.png" className="w-full h-full" alt="logo" />
+        <div className='flex-grow'>
+          <img src='/smit.png' className='w-full h-full' alt='logo' />
         </div>
         {/* right */}
-        <div className="flex-1">
-          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
-              <Label value="User Email" />
+              <Label value='User Email' />
               <TextInput
-                type="email"
-                placeholder="name@gmail.com"
-                id="email"
+                type='email'
+                placeholder='name@gmail.com'
+                id='email'
                 onChange={handleChange}
               />
             </div>
             <div>
-              <Label value="User Password" />
+              <Label value='User Password' />
               <TextInput
-                type="password"
-                placeholder="***********"
-                id="password"
+                type='password'
+                placeholder='***********'
+                id='password'
                 onChange={handleChange}
               />
             </div>
             <Button
-              gradientDuoTone="greenToBlue"
-              type="submit"
+              gradientDuoTone='greenToBlue'
+              type='submit'
               disabled={loading}
             >
               {loading ? (
                 <>
-                  <Spinner size="sm" />
-                  <span className="pl-3">Loading...</span>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
                 </>
               ) : (
-                "Sing In"
+                "Sign In"
               )}
             </Button>
             {/* <OAuth /> */}
           </form>
-          <div className="flex gap-2 text-sm mt-5">
+          <div className='flex gap-2 text-sm mt-5'>
             <span>Don't Have an account?</span>
-            <Link to="/sign-up" className="text-blue-500">
+            <Link to='/sign-up' className='text-blue-500'>
               Sign Up
             </Link>
           </div>
           {errorMessage && (
-            <Alert className="mt-5" color="failure">
+            <Alert className='mt-5' color='failure'>
               {errorMessage}
             </Alert>
           )}
